3b3d045485457a4fb1fa7d7bfa86a54308d63fb4
Nicolas <nlimadasilvabrito@gmail.com>
2024-07-01 21:51:42 -0300

    Classe implementa UserDetails para realizar autenticações

--- src/main/java/com/slgames/store/model/User.java
+++ src/main/java/com/slgames/store/model/User.java
@@ -1,5 +1,11 @@
 package com.slgames.store.model;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.slgames.store.dtos.users.InsertUserDTO;
 import com.slgames.store.dtos.users.UpdateUserDTO;
 import com.slgames.store.model.Role.RoleName;
@@ -24,8 +30,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -47,7 +57,7 @@ public class User {
 	}
 
 
-	public User(UpdateUserDTO dto) {
+	public void updateUser(UpdateUserDTO dto) {
 		setId(dto.id());
 		String nickname = dto.nickname();
 		String email = dto.email();
@@ -57,7 +67,60 @@ public class User {
 				&& !email.isEmpty()) setEmail(email);
 		if (nickname != null && !nickname.isBlank() && !nickname.isEmpty()) setNickname(nickname);
 		
-		if ( password != null && !password.isBlank() && !password.isEmpty()) setPassword(password);
+		if (password != null && !password.isBlank() && !password.isEmpty()) setPassword(password);
+	}
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		RoleName role = getRole().getRoleName();
+		switch (role) {
+			case ADM:
+				return List.of(getRole(), new Role(RoleName.STAFF), new Role(RoleName.DEFAULT));
+			case STAFF:
+				return List.of(getRole(), new Role(RoleName.DEFAULT));
+			default: 
+				return List.of(getRole());
+		}
+		
+	}
+
+
+	@Override
+	public String getUsername() {
+		return  getNickname() != null?getNickname(): getEmail();
+	}
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 	
 }
